6953b093230406d7ba425e55945eadbd9b2327ce
Daniel Marcelino de Lima <varnahal0712@gmail.com>
2022-06-11 15:43:14 -0300

    fiz alguns exercicios

--- CFB Cursos/aula02/ex001.js
+++ CFB Cursos/aula02/ex001.js
@@ -2,6 +2,8 @@ function aula(){
     var cores = [];
     cores['penis'] = 'azul';
     cores.push('amarelo'); 
-    cores.splice
+    cores.push('azul');
+    cores.push('ciano');
+    cores.splice(1,1);
     console.log(cores);
 }
