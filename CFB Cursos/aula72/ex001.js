0972c57f3a45a4e89e23620994653645e0291d05
Daniel Marcelino de Lima <varnahal0712@gmail.com>
2022-10-11 00:42:48 -0300

    dei uma estudada no spread novamente

--- CFB Cursos/aula72/ex001.js
+++ CFB Cursos/aula72/ex001.js
@@ -1,27 +1,19 @@
-// // let n1 = [10,20,30];
-// // let n2 = [11,22,33,44,55];
-// // let n3 = [...n1,...n2]
-// const obj = {nome:'Daniel',energia:100,vida:3}
-// const obj2 = {nome:'jose',energia:100,vida:5}
-// const obj3 = {...obj,...obj2}
+// // let n1 = [10,30,20,40,50];
+// // let n2 = [13,47,20,33,62];
+// // let n3 = [...n1,...n2];
+// const jogador1 = {'nome':'Daniel','energia':100,'vidas':3,'magia':150}
+// const jogador2 = {'nome':'Jose','energia':100,'vidas':3,'velocidade':80}
+// const jogador3 = {...jogador1,...jogador2}
 
-// console.log(obj3)
-// const soma = (...v3)=>{
-//     let sum = 0;
-//     for(v of v3){
-//         sum+=v;
-//     }
-//     return sum
-// };
-// let valores = [1,5,4,12,13]
+// // console.log(n1)
+// // console.log(n2)
+// console.log(jogador3)
+// const soma = (v1,v2,v3)=>{
+//     return v1+v2+v3
+// }
+// let val = [1,54,7]
+// console.log(soma(...val))//envia cada valor seprarado como um "for"
+let textos = document.getElementsByTagName('p');
+let textos2 = [...document.querySelectorAll('p')];
 
-// console.log(soma(...valores))
-const objs = document.getElementsByTagName('p');
-const objs2 = [...document.getElementsByTagName('p')];
-
-objs2.forEach(element=>{
-    console.log(element.outerHTML)
-})
-
-console.log(objs);
-console.log(objs2);
+console.log(textos)
